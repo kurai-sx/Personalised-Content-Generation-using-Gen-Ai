a600e9cbc3063d45a1c86aca84b5075ded6d80f2
Ganesh Kharde <khardeganeshn@gmail.com>
2024-08-01 16:19:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-
 import streamlit as st
-import google.generativeai as genai
+from openai import AzureOpenAI
+import os
+from dotenv import load_dotenv
 
-model = genai.GenerativeModel('gemini-pro')
+load_dotenv()
 
-#API Key
-genai.configure(api_key="Gemini API Key")
+API_KEY = os.getenv('API_KEY')
+
+client = AzureOpenAI(
+    api_key=os.environ["API_KEY"],
+    api_version="2024-02-01",
+    azure_endpoint="https://teamstrawhats.openai.azure.com/"
+)
+
+
+
+# This will correspond to the custom name you chose for your deployment when you deployed a model.
+# Use a gpt-35-turbo-instruct deployment.
+deployment_name = "strawhacksproject"
+
+
 
 st.set_page_config(
     page_title="Personalised Content Generator",
@@ -17,6 +31,7 @@ st.columns(3)[1].image("img.png")
 
 st.title('Personalised Content Generator')
 st.sidebar.subheader("Select the parameter:",divider='rainbow')
+
 
 
 # type = st.text_input("Enter the Type")
@@ -33,6 +48,8 @@ content_type = st.sidebar.radio(
     ["Post Caption", "Image (Coming Soon!)", "Comment", "Recommendation"],index=None,
 )
 
+
+
 if (content_type == "Image (Coming Soon!)"):
     st.toast('Image Generation is not Supported Yet!', icon='🚫')
     st.sidebar.error("Can't Generate Image!, Select other Content Type")
@@ -48,16 +65,30 @@ content_level = st.sidebar.radio(
     ["Creative", "Descriptive", "Logical", "Joyful"],index=None,
 )
 
+
+
+#load model
 def load_model(prompt):
     with st.spinner('Generating the Content...'):
-        response = model.generate_content(prompt)
+        #model calling
+        # response = model.generate_content(prompt)
+        result = client.completions.create(
+            model=deployment_name,
+            prompt=prompt,
+            temperature=1,
+            max_tokens=350,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+            stop=None
+        )
 
 
-    if response.text:
-        output = response.text
+    if result.choices[0]:
+        output = result.choices[0].text
         return output
     else:
-        st.error("Sorry, but I think Gemini didn't want to answer that!")
+        st.error("Sorry, but I think Azure OpenAI didn't want to answer that!")
 
 try:
     if st.button("Generate", type="primary"):
@@ -65,9 +96,9 @@ try:
 
             if (content_type != "Image (Coming Soon!)"):
 
-                prompt = "You are a professional in personalised content generation and you can generate any social media content based on the given data." \
+                prompt = "You are a professional in personalised content (content is generation and you can generate any social media content based on the given data." \
                           "generate a "+ str(content_type) + "for a post related to the " + str(context)+" goal of the post is " + str(goal) + " give in only "+ str(output_size)+ " words" \
-                        "the "+ str(content_type) + " is going to be used for posting on the platform " + str(platform) + "the content should be "+ str(content_level)
+                        "the "+ str(content_type) + " is going to be used for posting on the platform " + str(platform) + "constraint is give me the content according to the prompt dont give me the content out of the concept. give me only content, dont give explanation and description. give content only in "+ str(output_size) +" words only"
 
 
                 output = load_model(prompt)
@@ -94,6 +125,14 @@ except Exception as e:
     print("Sorry, but Gemini didn't want to answer that!")
     st.error("Sorry, but Model didn't want to answer that!")
 
+
+
+
+footer_html = """<div style='text-align: center;'>
+  <p>Developed with ❤️ by Team Straw Hats</p>
+</div>"""
+
+st.markdown(footer_html, unsafe_allow_html=True)
 
 
 
